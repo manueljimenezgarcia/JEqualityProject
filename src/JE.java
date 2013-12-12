6067641c2fed3148b50014a23dc0cc0ed53e366f
Manuel Jimenez <manuel.jimenez@buongiorno.com>
2013-12-12 10:32:54 +0100

    Main de JE

---
+++ src/JE.java
@@ -0,0 +1,25 @@
+public class JE {
+
+	public static void main(String[] args) {
+		
+		Puticlub puticlub = new Puticlub();
+		Cerveza cerveza = new Cerveza();
+		Siesta siesta = new Siesta();
+		Tapa tapa = new Tapa();
+
+		cerveza.beber();
+		cerveza.rellenar();
+		cerveza.beber();
+
+		for(int i = 0; i < 4; i++) {
+			tapa.dameTapa(i);
+		}
+
+		puticlub.getChica(0);
+
+		System.out.println("Ay que cansao estoy, mi arrrma!!!");
+
+		siesta.echar();
+	}
+
+}
